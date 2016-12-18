f9369a302f33426fe09f30dcc2b855a544c69ecd
William S. Warren <wswarren12@Williams-Air.fios-router.home>
2016-12-18 13:37:40 -0500

    Adding User Summary

--- models/User.js
+++ models/User.js
@@ -6,7 +6,24 @@ var UserSchema = new mongoose.Schema({
   username: {type: String, lowercase: true, trim: true, default: ''},
   email: {type: String, lowercase: true, trim: true, default: ''},
   password: {type: String, default: ''},
-  timestamp: {type: String, default: Date.now},
+  image: {type: String, trim: true, default: ''},
+  timestamp: {type: String, default: Date.now}
 })
+
+
+UserSchema.methods.summary = function (){
+  var summary = {
+    firstName: this.firstName,
+    lastName: this.lastName,
+    username: this.username,
+    email: this.email,
+    timestamp: this.timestamp,
+    image: this.image,
+    id: this._id
+  }
+
+  return summary
+
+}
 
 module.exports = mongoose.model('UserSchema', UserSchema)
